81f80d46cfa0c7383c728545a78f4ebe69b965fb
quebsilva16 <55449865+quebsilva16@users.noreply.github.com>
2020-04-21 20:49:22 -0500

    Add files via upload
    Proyecto Final

---
+++ Reporte 1/cmake-build-debug/CMakeFiles/Reporte_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Reporte_1.dir/main.cpp.obj"
+  "Reporte_1.exe"
+  "Reporte_1.exe.manifest"
+  "Reporte_1.pdb"
+  "libReporte_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Reporte_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
